cb784cfe3bb6179eba872e99473590c5c6cbabaa
Martins Nwanakwere <nwa23012@byui.edu>
2023-09-14 14:49:37 -0700

    Debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,24 +5,18 @@
 * Return: 0
 */
 int main(void)
-{
-	/* Declare variables */
+{	
 	int i;
-
-	/* Print a message */
 	printf("Infinite loop incoming :(\n");
 
-	/* Initialize i */
 	i = 0;
 
-	/* Infinite loop */
-	while (i < 10)
-	{
-		putchar(i);
-		i++; /* Increment i */
-	}
-
-	/* Print a message */
+	/**
+	 * while (i < 10)
+	*{
+	*	putchar(i);
+	*}
+	*/
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
